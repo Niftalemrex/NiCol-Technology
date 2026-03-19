581e5079f2b337773cd3583562d58b3cbac64cc6
Niftalem Awel <niftalemawel@gmail.com>
2026-03-19 20:33:35 +0300

    Update: fixed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://nicol-technology.vercel.app',
-    generateRobotsTxt: true,
+    generateRobotsTxt: true, // (optional)
+    sitemapSize: 5000,
+    changefreq: 'daily',
+    priority: 0.7,
   };
